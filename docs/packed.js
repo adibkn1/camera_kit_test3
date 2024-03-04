d5a5208a8b9c6a0d0b58e58828a5f09618f6e8d9
adibkn1 <55048595+adibkn1@users.noreply.github.com>
2024-03-04 20:35:29 +0530

    Update packed.js

--- docs/packed.js
+++ docs/packed.js
@@ -35714,8 +35714,8 @@ function createImageSource(image, options = {}) {
 
 // Define your target location coordinates
 const targetLocation = {
-  latitude: 28.451266409859592,  // Replace with your target latitude
-  longitude: 77.09719998169876, // Replace with your target longitude
+  latitude: 25.254148963470538,   // Replace with your target latitude
+  longitude: 46.3870724557909, // Replace with your target longitude
 };
 
 function calculateDistance(lat1, lon1, lat2, lon2) {
@@ -35833,7 +35833,7 @@ function checkLocationAndInit() {
     }, function(error) {
       alert(`ERROR(${error.code}): ${error.message}`);
     }, {
-      maximumAge: 60000,
+      maximumAge: 5000,
       timeout: 5000,
       enableHighAccuracy: true
     });
